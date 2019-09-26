07433452a2b5398205e499a79e18a96bc734b4d6
Ahmad Yunus <venahy@sg.uob>
2019-09-26 16:10:59 +0800

    Setup Generic adapter

---
+++ app/src/main/java/com/yunus/moviedb/feature/common/TaskDiffCallback.kt
@@ -0,0 +1,13 @@
+package com.yunus.moviedb.feature.common
+
+class TaskDiffCallback<T> : androidx.recyclerview.widget.DiffUtil.ItemCallback<T>() {
+    override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+        return oldItem.hashCode() == newItem.hashCode()
+    }
+
+    override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+        return oldItem.hashCode() == newItem.hashCode()
+    }
+}
+
+
